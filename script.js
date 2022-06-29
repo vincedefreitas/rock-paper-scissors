2ef4fec9136027a81b7ef3ec4f0efb800bb66202
Vince <vince.defreitas@gmail.com>
2022-06-29 15:39:13 +0100

    Add prompt for user input

---
+++ script.js
@@ -0,0 +1,31 @@
+// Create function called computerPlay() that randomly returns Rock Paper or Scissors
+
+// Write playRound() function that plays single round of rock paper scissors
+
+// function must contain the parameters playerSelection and computerSelection
+
+// return string that declares winner of round
+
+// playerSelection should be case insensitive
+
+// Write new function called game() - call playRound function inside of this to play a 5
+// round game that keeps score and reports a winner or loser at end (hint: loops)
+// for (let i = 0; i < 5; i++;)
+
+// display result at end of each round and winner at the end
+
+// prompt user for input
+
+let playerSelection = prompt("Rock, Paper or Scissors? ")
+
+
+
+
+
+// prompt user for input
+// computer to select random from rock, paper, scissors
+// compare user selection against computer selection
+// tell user if they have won or lost round
+// variable to keep track of score
+// while the score is less than 5, keep playing
+// repeat until score reaches 5, tell user if they won or lost
