7a2923dc3b702e5a6621a1608e80cb9d54335a24
Vince <vince.defreitas@gmail.com>
2022-07-01 12:31:43 +0100

    Solve win message bug

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
-// Keep score
-
 let playerScore = 0
 let computerScore = 0
 
-// Create function called computerPlay() that randomly returns Rock Paper or Scissors
+console.log("Welcome to Rock Paper Scissors! Best of 5")
 
 function computerPlay() {
     let choice = ["rock", "paper", "scissors"];
     let computerSelection = choice[Math.floor(Math.random() * choice.length)];
     return computerSelection;
 }
-
-// Write playRound() function that plays single round of rock paper scissors
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === "rock" && computerSelection === "paper") {
@@ -32,15 +28,12 @@ function playRound(playerSelection, computerSelection) {
     } else if (playerSelection === "paper" && computerSelection === "rock") {
         playerScore += 1
         return "You win! Paper beats Rock";
-    } else {
+    } else if (playerSelection === computerSelection) {
         return "It's a draw! Go again";
+    } else {
+        return "You have chosen an option that isn't there. Choose again"
     }
 }
-
-
-// Write new function called game() - call playRound function inside of this to play a 5
-// round game that keeps score and reports a winner or loser at end (hint: loops)
-// for (let i = 0; i < 5; i++;)
 
 function game() {
     for (let i = 0; i < 10; i++) {
@@ -51,32 +44,13 @@ function game() {
             console.log(`Computer chose ${computerSelection}`)
             console.log(playRound(playerSelection, computerSelection))
             console.log(`Player Score: ${playerScore} Computer Score: ${computerScore}`)
-        } else if (computerScore === 3) {
-            console.log("Computer wins! Commiserations")
-        } else {
-            console.log("You win! Congratulations!")
-        }
+        } 
     }
-    
+    if (computerScore === 3) {
+        console.log("Computer wins! Commiserations")
+    } else {
+        console.log("You win! Congratulations!")
+    }
 }
 
 game()
-
-
-// display result at end of each round and winner at the end
-
-// prompt user for input
-
-// let playerSelection = prompt("Rock, Paper or Scissors? ")
-
-
-
-
-
-// prompt user for input
-// computer to select random from rock, paper, scissors
-// compare user selection against computer selection
-// tell user if they have won or lost round
-// variable to keep track of score
-// while the score is less than 5, keep playing
-// repeat until score reaches 5, tell user if they won or lost
